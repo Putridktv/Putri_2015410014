a995e4e589c5479ca7c87429a951fb1dcf301b6b
Putridktv <90168345+Putridktv@users.noreply.github.com>
2021-10-19 21:24:12 +0700

    Index Putri

--- Index.php
+++ Index.php
@@ -1,5 +1,6 @@
 <?php
 echo "<h2> Selamat berlatih menjadi kolaborator </h2>";
 echo "<h3>Isi Nama dan Nim Anda di bawah baris code ini</h3>";
-echo "<h3>YUSSENO (205410050)</h3>"
+echo "<h3>Putri Dwi Oktaviani (205410014)</h3>"
+
 ?>
